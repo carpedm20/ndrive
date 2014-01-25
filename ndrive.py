56888646dfa8137cf9f7d424c5f00f73ecc1274f
Kim Tae Hoon <carpedm20@HeXA-Moza.(none)>
2014-01-25 22:58:45 +0900

    login added

--- ndrive.py
+++ ndrive.py
@@ -12,36 +12,60 @@ Getting started
 Copyright
 ---------
 
-Copyright © 2014 Kim Tae Hoon.
+ndrive is following an MIT License.
 
 """
 
 import mechanize
 import urllib2
 import json
-from login import Login
+from login import naverLogin
 class ndrive:
-    def __init__(self, user_id = None, password = None):
-    """Initialize ndrive instance
+    def __init__(self, NID_AUT = None, NID_SES= None):
+        """Initialize ndrive instance
 
-    Using given user information, login to ndrive server and create a session
+        Using given user information, login to ndrive server and create a session
 
-    Args:
-        user_id: Naver login id
-        password: Naver login password
+        Args:
+            NID_AUT : Naver account authentication info
+            NID_SES : Naver account session info
 
-    Returns:
+        Returns:
 
-
-    Remarks:
-        self.cookie is a dictionary with 5 keys: path, domain, NID_AUT, nid_inf, NID_SES
     """
-        if user_id == None || password == None:
-            print "Error __init__: user_id and password is needed"
-            return None
-            
-        self.user_id = user_id
+        self.NID_AUTH = NID_AUT
+        self.NID_SES = NID_SES
+
+    def login(self, userId, password):
+        """Log in Naver and get cookie
+
+        Agrs:
+            user_id: Naver account's login id
+            password: Naver account's login password
+
+        Returns:
+            True : Login success
+            False : Login failed
+
+        Remarks:
+            self.cookie is a dictionary with 5 keys: path, domain, NID_AUT, nid_inf, NID_SES
+        """
+        self.userId = userId
         self.password = password
 
-        login = Login(userId, password)
-        self.cookie = login.cookie
+        if self.userId == None or self.password == None:
+            print "Error __init__: user_id and password is needed"
+            return None
+
+        try:
+            cookie = naverLogin(userId, password)
+        except:
+            return False
+
+        #self.cookie = cookie
+        self.NID_AUT = cookie["NID_AUT"]
+        self.NID_SES = cookie["NID_SES"]
+            
+        return True
+
+    
