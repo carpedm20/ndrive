84a151fd2e6211f030c6cf251cfb2c6b82cdd916
carpedm20@gmail.com <carpedm20@gmail.com>
2014-01-27 04:06:05 +0900

    add bunch or urls and functions

--- ndrive/models.py
+++ ndrive/models.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """
 ndrive
 ======
@@ -29,8 +32,8 @@ from .urls import ndrive_urls as nurls
 class FileInfo(object):
 
     #: Security protect
-    #: Y: protected
-    #: N: not protected
+    #: Y: protected 중요 표시
+    #: N: not protected 
     protect = None
 
     #: Resource number
@@ -116,46 +119,6 @@ class FileInfo(object):
     viewWidth = None
     viewHeight = None
 
-    def __repr__(self):
-        return '[%s] resourceno: %s, resourcetype: %s' % (
-            self.href.encode('utf-8'),
-            self.resourceno,
-            self.resourcetype,
-        )
-
-    def setJson(self, j):
-        self.json = j
-
-class FileProperty(object):
-    #: File href
-    #: ex) /Photo/FILE_NAME
-    href = None
-
-    #: Resource type
-    #: ex) property
-    resourcetype = None
-
-    #: Resource Number
-    resourceno = None
-
-    #: Creation date
-    #: ex) 2014-01-25T18:59:59+09:00
-    creationdate = None
-
-    #: Last accessed date
-    #: ex) 2014-01-25T18:59:59+09:00
-    lastaccessed = None
-
-    #: Last modified date
-    #: ex) 2014-01-25T18:59:59+09:00
-    getlastmodified = None
-
-    #: Byte sized content length
-    getcontentlength = None
-
-    #: Protected or not
-    protect = None
-
     #: Total file count
     #: Integer number
     totalfilecnt = None
@@ -165,46 +128,37 @@ class FileProperty(object):
     totalfoldercnt = None
 
     #: File type
-    #: 1: 
-    #: 2: jpg
+    #: 1: document, 문서 파일
+    #: 2: image, 사진 파일
+    #: 3: video, 동영상 파일
+    #: 4: music, 음악 파일
+    #: 5: zip, 압축 파일
     #: 
     filetype = None
 
     #: Exif
-    #: Y:
-    #: N:
+    #: Y: Yes exif info
+    #: N: No exif info
     exif = None
 
     #: Has thumbnail
-    #: Y:
-    #: N:
+    #: Y: Has thumbnail
+    #: N: No thumbnail
     thumbnail = None
-
-    #: File upload status
-    #: 1: default
-    fileuploadstatus = None
-
-    #: Virus status
-    #: Y:
-    #: N:
-    virusstatus = None
-
-    #: File link
-    #: Y:
-    #: N: Has no file link
-    filelink = None
 
     #: File link ulr
     #: null:
     filelinkurl = None
 
-"""
-    resultcode & message:
-        0 = success
-        36 = File Infomation Not Found
-        2002 = Invalidation Cookie
-        
-"""
+    def __repr__(self):
+        return '[%s] resourceno: %s, resourcetype: %s' % (
+            self.href.encode('utf-8'),
+            self.resourceno,
+            self.resourcetype,
+        )
+
+    def setJson(self, j):
+        self.json = j
 
 class ndrive:
     # requests session
@@ -376,15 +330,8 @@ class ndrive:
                 'uploadsize': file_size,
                }
         r = self.session.post(nurls['getDiskSpace'], data = data)
-        j = json.loads(r.text)
 
-        if j['message'] != 'success':
-            print "[*] Error getRegisterUserInfo: " + j['message']
-            return False
-
-        else:
-            self.useridx = j['resultvalue']['useridx']
-            return True
+        return resultManager(r.text)
 
     def checkUpload(self, file_path, upload_path = '', overwrite = False):
         """Check upload
@@ -415,15 +362,8 @@ class ndrive:
                 }
 
         r = self.session.post(url = url, data = data)
-        j = json.loads(r.text)
 
-        if j['message'] != 'success':
-            print '[*] Error checkUpload: ' + j['message']
-
-            return False
-        else:
-            print '[*] Success checkUpload'
-            return True
+        return self.resultManager(r.text)
 
     def put(self, file_path, upload_path = ''):
         """PUT
@@ -454,13 +394,8 @@ class ndrive:
                    'Origin': 'http://ndrive2.naver.com',
         }
         r = self.session.put(url = url, data = c, headers = headers)
-        j = json.loads(r.text)
 
-        if j['message'] is 'success':
-            return True
-        else
-            print '[*] Error delete: ' + j['message']
-            return False
+        return self.resultManager(r.text)
 
     def delete(self, file_path):
         """DELETE
@@ -485,13 +420,8 @@ class ndrive:
                    'Origin': 'http://ndrive2.naver.com',
         }
         r = self.session.delete(url = url, headers = headers)
-        j = json.loads(r.text)
 
-        if j['message'] is 'success':
-            return True
-        else
-            print '[*] Error delete: ' + j['message']
-            return False
+        return self.resultManager(r.text)
 
     def getList(self, dummy = 56184, orgresource = '/', type = 1, dept = 0, sort = 'name', order = 'asc', startnum = 0, pagingrow = 1000):
         """GetList
@@ -509,14 +439,20 @@ class ndrive:
                   5 => directories and files without thumbnail info
 
             depth: Dept for file list
-            sort: Sort criteria (name, credate ...)
+            sort: name => 이름
+                  file => file type, 종류
+                  length => size of file, 크기
+                  date => edited date, 수정한 날짜
+                  credate => creation date, 올린 날짜
+                  protect => protect or not, 중요 표시
+
             order: Order by (asc, desc)
             startnum: ???
             pagingrow: start index ?
 
         Returns:
-            True: Upload success
-            False: Upload failed
+            FileInfo list: List of files for a path
+            False: Failed to get list
 
         """
         
@@ -582,7 +518,7 @@ class ndrive:
 
             return files
 
-    def doMove(self, dummy = 56184, orgresource, dstresource, dstresource = 'F', bShareFireCopy = 'false'):
+    def doMove(self, orgresource, dstresource, dummy = 56184, stresource = 'F', bShareFireCopy = 'false'):
         """DoMove
 
         Args:
@@ -604,7 +540,7 @@ class ndrive:
                 'dummy': dummy,
                 'orgresource': orgresource,
                 'dstresource': dstresource,
-                'overwrite': overwrite
+                'overwrite': overwrite,
                 'bShareFireCopy': bShareFireCopy,
                 }
 
@@ -613,17 +549,13 @@ class ndrive:
         try:
             j = json.loads(r.text)
         except:
-            #print '[*] Success checkUpload: 0 result'
+            print '[*] Success checkUpload: 0 result'
 
             return False
 
-        if j['message'] is 'success':
-            return True
-        else
-            print '[*] Error doMove: ' + j['message']
-            return False
+        return self.resultManager(r.text)
 
-    def getProperty(self, dummy = 56184, orgresource):
+    def getProperty(self, orgresource, dummy = 56184):
         """GetProperty
 
         Args:
@@ -631,7 +563,7 @@ class ndrive:
             orgresource: File path
 
         Returns:
-            JSON_FILE: Property of file
+            FileInfo object:
             False: Failed to get property
 
         """
@@ -645,16 +577,154 @@ class ndrive:
                 }
 
         r = self.session.post(url = url, data = data)
+        j = json.loads(r.text)
 
-        try:
-            j = json.loads(r.text)
-        except:
-            #print '[*] Success checkUpload: 0 result'
+        if self.resultManager(r.text):
+            f = FileInfo()
+            result = j['resultvalue']
 
+            f.resourcetype = result['resourcetype']
+            f.resourceno = result['resourceno']
+
+            return f
+
+        else:
             return False
 
-        if j['message'] is 'success':
+    def getVersionList(self, orgresource, startnum = 0, pagingrow = 50):
+        """GetVersionList
+
+        Args:
+            orgresource: File path
+            startnum: Start version index
+            pagingrow: Max # of version list
+
+        Returns:
+            FileInfo object:
+            False: Failed to get property
+
+        """
+
+        url = nurls['getVersionList']
+
+        data = {'userid': self.user_id,
+                'useridx': self.useridx,
+                'orgresource': orgresource,
+                'startnum': startnum,
+                'pagingrow': pagingrow,
+                }
+
+        r = self.session.post(url = url, data = data)
+        j = json.loads(r.text)
+
+        if self.resultManager(r.text):
+            f = FileInfo()
+            result = j['resultvalue']
+
+            f.resourcetype = result['resourcetype']
+            f.resourceno = result['resourceno']
+            ######### ??????
+
+            return f
+
+        else:
+            return False
+
+    def getVersionListCount(self, orgresource):
+        """GetVersionListCount
+
+        Args:
+            orgresource: File path
+
+        Returns:
+            Integer number: # of version list
+            False: Failed to get property
+
+        """
+
+        url = nurls['getVersionListCount']
+
+        data = {'userid': self.user_id,
+                'useridx': self.useridx,
+                'orgresource': orgresource,
+                }
+
+        r = self.session.post(url = url, data = data)
+        j = json.loads(r.text)
+
+        if j['message'] != 'success':
+            print "[*] Error getVersionListCount: " + j['message']
+            return False
+        else:
+            return int(j['resultvalue']['count'])
+
+    def setProperty(self, orgresource, protect, dummy = 7046):
+        """SetProperty
+
+        Args:
+            orgresource: File path
+            protect: 'Y' or 'N', 중요 표시
+
+        Returns:
+            Integer number: # of version list
+            False: Failed to get property
+
+        """
+
+        url = nurls['setProperty']
+
+        data = {'userid': self.user_id,
+                'useridx': self.useridx,
+                'orgresource': orgresource,
+                'protect': protect,
+                'dummy': dummy,
+                }
+
+        r = self.session.post(url = url, data = data)
+
+        return resultManager(r.text)
+
+    def getMusicAlbumList(self, tagtype = 0, startnum = 0, pagingrow = 100):
+        """GetMusicAlbumList
+
+        Args:
+            tagtype = ???
+            startnum
+            pagingrow
+
+        Returns:
+            ???
+            False: Failed to get property
+
+        """
+
+        url = nurls['setProperty']
+
+        data = {'userid': self.user_id,
+                'useridx': self.useridx,
+                'tagtype': tagtype,
+                'startnum': startnum,
+                'pagingrow': pagingrow,
+                }
+
+        r = self.session.post(url = url, data = data)
+
+        return resultManager(r.text) # need to edit
+
+    def resultManager(self, text):
+        """
+        resultcode & message:
+            0 = success => if "resultvalue" = null: no result to show
+            11 = Not Exist Path
+            36 = File Infomation Not Found
+            2002 = Invalidation Cookie
+        
+        """
+        j = json.loads(text)
+
+        if j['message'] != 'success':
+            print "[*] Error : " + j['message']
+            return False
+        else:
             return True
-        else
-            print '[*] Error doMove: ' + j['message']
-            return False
+
--- ndrive/urls.py
+++ ndrive/urls.py
@@ -171,6 +171,8 @@ ndrive_urls['getList'] = base_url + "/GetList.ndrive"
 
 """POST /GetWasteInfo.ndrive
 
+Information about waste, 휴지동 크기
+
 Params:
     userid=carpedm20
     useridx=6794877
@@ -187,6 +189,78 @@ Returns:
     }
 """
 ndrive_urls['getWasteInfo'] = base_url + "/GetWasteInfo.ndrive"
+
+"""POST /GetWasteListCount.ndrive
+
+Params:
+    userid=carpedm20
+    useridx=6794877
+    dummy=47304
+    ownerid=
+    owneridx=
+
+Returns:
+    {
+        "resultcode":0,
+        "message":"success",
+        "resultvalue":{
+            "count":2
+        }
+    }
+"""
+ndrive_urls['getWasteListCount'] = base_url + "/GetWasteListCount.ndrive"
+
+"""POST /GetWasteList.ndrive
+
+File list in waste, 휴지동 파일 리스트
+
+Params:
+    userid=carpedm20
+    useridx=6794877
+    dummy=27764
+    sort=deletedate
+    order=desc
+    startnum=0
+    pagingrow=100
+    type=3
+
+Returns:
+        {
+        "resultcode":0,
+        "message":"success",
+        "resultvalue":[
+            {
+                "href":"FILE_PATH",
+                "resourcetype":"property",
+                "getlastmodified":"2014-01-26T02:16:29+09:00",
+                "getcontentlength":66247,
+                "deletedate":"2014-01-26T17:29:03+09:00",
+                "resourceno":12457657175,
+                "physhref":"\/.recycled\/ndrive_-427244713",
+                "orgpath":"\/"
+            },
+            ...
+        ]
+    }
+"""
+ndrive_urls['getWasteList'] = base_url + "/GetWasteList.ndrive"
+
+"""DELETE /PHYSHREF
+
+    ex) /.recycled/solar.png_-427837829
+    ex) / => Empty waste
+
+Completely delete a file
+
+Params:
+
+Returns:
+    {
+        "resultcode":0,
+        "message":"success"
+    }
+"""
+ndrive_urls['deleteWasteFile'] = base_url
 
 """POST /DoMove.ndrive 
 
@@ -232,7 +306,7 @@ Params:
     userid=carpedm20
     useridx=6794877
     dummy=38404
-    orgresource=%2Fsolar2.png
+    orgresource=FILE_PATH
 
 Returns:
     {
@@ -241,3 +315,182 @@ Returns:
     }
 """
 ndrive_urls['getProperty'] = base_url + "/GetProperty.ndrive"
+
+"""POST /GetVersionList.ndrive
+
+Params:
+    userid=carpedm20
+    useridx=6794877
+    orgresource=FILE_PATH
+    startnum=0
+    pagingrow=50
+
+Returns:
+    {
+        "resultcode":0,
+        "message":"success",
+        "resultvalue":[
+            {
+                "href":"FILE_PATH",
+                "getlastmodified":"2014-01-26T13:11:57+09:00",
+                "versioninfo":"O",
+                "versionkey":32401988,
+                "filesize":97656,
+                "createuser":"carpedm20"
+            }
+        ]
+    }
+"""
+ndrive_urls['getVersionList'] = base_url + "/GetVersionList.ndrive"
+
+"""POST /GetVersionListCount.ndrive
+
+Params:
+    userid=carpedm20
+    useridx=6794877
+    orgresource=FILE_PATH
+    
+Returns:
+    {
+        "resultcode":0,
+        "message":"success",
+        "resultvalue":{
+            "count":1
+        }
+    }
+"""
+ndrive_urls['getVersionListCount'] = base_url + "/GetVersionListCount.ndrive"
+
+"""POST /SetProperty.ndrive
+
+Params:
+    userid=carpedm20
+    useridx=6794877
+    dummy=7046
+    orgresource=FILE_PATH
+    protect=Y
+
+Returns:
+    {
+        "resultcode":0,
+        "message":"success",
+        "resultvalue":{
+            "href":"FILE_PATH",
+            "protect":"Y"
+        }
+    }
+"""
+ndrive_urls['setProperty'] = base_url + "/SetProperty.ndrive"
+
+"""POST /GetMusicAlbumList.ndrive
+
+Params:
+    userid=carpedm20
+    useridx=6794877
+    dummy=84125
+    tagtype=0
+    startnum=0
+    pagingrow=100
+
+Returns:
+    {
+        "resultcode":0,
+        "message":"success",
+        "resultvalue":null
+    }
+"""
+ndrive_urls['getMusicAlbumList'] = base_url + "/GetMusicAlbumList.ndrive"
+
+"""POST /DoSearch.ndrive
+
+Params:
+    userid=carpedm20
+    useridx=6794877
+    dummy=46209
+    sharedowner=P
+        ex) P: priority files only, A: all files
+    sort=update
+        ex) update, date, 
+    order=desc
+    startnum=0
+    pagingrow=100
+    includeworks=Y
+    type=3
+
+    # bellow is for query search
+    filename=123
+    filetype=0
+    dstresource=%2F
+    bodysearch=N
+    highlighttag=%3Cem%20style%3D%22color%3A%23019a30%22%3E%24SEARCH%3C%2Fem%3E
+
+    #
+    startdate=2014-01-13T00%3A00%3A00%2B90%3A00
+    enddate=2014-01-27T23%3A59%3A59%2B09%3A00
+
+Returns:
+        {
+        "resultcode":0,
+        "message":"success",
+        "resultvalue":[
+            {
+                "href":"\/OS-01장.doc",
+                "owner":"carpedm20",
+                "resourceno":12444576841,
+                "getcontentlength":136192,
+                "protect":"Y",
+                "resourcetype":"property",
+                "creationdate":"2014-01-16T17:24:42+09:00",
+                "getlastmodified":"2014-01-16T17:24:42+09:00",
+                "lastaccessed":"2014-01-16T17:24:42+09:00",
+                "fileuploadstatus":"1",
+                "thumbnailpath":"N",
+                "shareno":0,
+                "root_shareno":0,
+                "sharedinfo":"F",
+                "ownerid":"carpedm20",
+                "owneridx":6794877,
+                "owneridc":"",
+                "ownership":"W",
+                "root":"",
+                "sharedfoldername":"",
+                "subpath":"",
+                "s_type":"normal",
+                "copyright":"N",
+                "virusstatus":"N",
+                "authtoken":"",
+                "filelink":"N",
+                "content":"",
+                "hilightfilename":"OS-01장.doc",
+                "domaintype":""
+            }
+        ]
+    }
+"""
+naver_urls['doSearch'] = base_url + "/DoSearch.ndrive"
+
+"""POST /DoSearchCount.ndrive
+
+Params:
+    userid=carpedm20
+    useridx=6794877
+    dummy=36644
+    filetype=2
+    filename=
+    dstresource=%2F
+    sharedowner=A
+    datetype=all
+    searchtype=filesearch
+    includeworks=Y
+    bodysearch=N
+
+Return:
+    {
+        "resultcode":0,
+        "message":"success",
+        "resultvalue":{
+            "count":9883
+        }
+    }
+"""
+naver_urls['doSearchCount'] = base_url + "/DoSearchCount.ndrive"
