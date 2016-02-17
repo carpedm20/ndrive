f1a23a6a83e77f592f21b2bfd6d19df2c1880eaf
lexifdev <sl@lxf.kr>
2016-02-18 00:24:05 +0900

    simplify authentication process by requests

--- ndrive/auth.py
+++ ndrive/auth.py
@@ -10,9 +10,9 @@ import requests
 import urllib, base64, rsa, httplib, re
 import spidermonkey
 
+
 def getCookie(id, password):
     f = urllib.urlopen('http://static.nid.naver.com/enclogin/keys.nhn')
-
     keystring = f.read()
     f.close()
 
@@ -34,9 +34,10 @@ def getCookie(id, password):
         upw = '%s';
         encrypted = rsa.encrypt(getLenChar(sessionkey)+sessionkey\
                     +getLenChar(uid)+uid+getLenChar(upw)+upw);
-        ''' %(keystring, id, password))
+        ''' % (keystring, id, password))
 
     keyname, encpw = str(cx.execute('keyname')), str(cx.execute('encrypted'))
+
     params = dict(enctp='1',
                   encnm=keyname,
                   svctype='0',
@@ -45,44 +46,14 @@ def getCookie(id, password):
                   url='www.naver.com',
                   smart_level='1',
                   encpw=encpw)
-    params = urllib.urlencode(params)
 
-    headers = {'Content-type': 'application/x-www-form-urlencoded',
-               'Accept':'text/plain'}
-    conn = httplib.HTTPSConnection('nid.naver.com')
-    conn.request('POST', '/nidlogin.login', params, headers)
-    response = conn.getresponse()
-    data = response.read()
-    conn.close()
-    cookie = response.getheader('set-cookie')
+    session = requests.Session()
+    response = session.post('https://nid.naver.com/nidlogin.login', data=params)
 
-    while True:
-        headers = {'Accept':'text/plain', 'Cookie':cookie}
-        if response.status == 302:
-            location = response.getheader('location')
-            host, url = re.match(r'http:\/\/([\w+\.]*)(.*)', location).groups()
-        elif response.status == 200:
-            host, url = re.search(r'"(?:http:\/\/)?([\w+\.]*)(.*)"', data).groups()
-        conn = httplib.HTTPConnection(host)
-        conn.request('GET', url, '', headers)
-        response = conn.getresponse()
-        data = response.read()
-        if response.status == 302:
-            cookie = response.getheader('set-cookie').replace(';, ', '; ')
-        conn.close()
-        if host == 'www.naver.com':
-            break
+    finalize_url = re.search(r'location\.replace\("([^"]+)"\)', response.content).group(1)
+    session.get(finalize_url)
 
-    cookies = [c.split('=',1) for c in cookie.split(';')]
-
-    cookie = {}
-    for c in cookies:
-        try:
-            key = c[0].strip()
-            value = c[1].strip()
-
-            cookie[key] = value
-        except:
-            continue # do nothing
-
-    return cookie
+    return {
+        'NID_AUT': session.cookies.get('NID_AUT'),
+        'NID_SES': session.cookies.get('NID_SES')
+    }
